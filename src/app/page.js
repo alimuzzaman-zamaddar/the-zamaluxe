bd80ede8ce5ce93cac8378bbea65c31af459b1be
ALIMUZZAMAN <alimuzzamanzamaddar@gmail.com>
2023-08-28 18:10:01 +0600

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
 import Banner from '@/components/Banner'
 import CardSection from '@/components/CardScetion'
+import Dessert from '@/components/Dessert'
+import Entertainment from '@/components/Entertainment'
+import Explore from '@/components/Explore'
+import Footer from '@/components/Footer'
+import Fountain from '@/components/Fountain'
+import Gallery from '@/components/Gallery'
 import Navbar from '@/components/Navbar'
+import Resort from '@/components/Resort'
 import Image from 'next/image'
 
 export default function Home() {
@@ -9,7 +16,13 @@ export default function Home() {
       <Navbar></Navbar>
       <Banner></Banner>
       <CardSection></CardSection>
-    
+      <Resort></Resort>
+      <Entertainment></Entertainment>
+      <Fountain></Fountain>
+      <Explore></Explore>
+      <Gallery></Gallery>
+      <Dessert></Dessert>
+      <Footer></Footer>
     </main>
   )
 }
---
+++ src/components/Explore.jsx
@@ -0,0 +1,27 @@
+const Explore = () => {
+    return (
+      <div>
+        <div className="relative">
+          <div className="">
+            <img
+              className="h-screen w-full "
+              data-aos="zoom-in"
+              data-aos-duration="3000"
+              src="https://assets.kerzner.com/api/public/content/b480e7798a324103a123a3a6d651b2bc?v=ee76577f&t=w1920"
+              alt=""
+            />
+          </div>
+          <div className=" absolute top-[37%] left-[10%] w-[500px] mb-3 bg-[#201b0d] bg-opacity-30"  data-aos="fade-right"
+              data-aos-duration="3000">
+            <h1 className=" text-6xl font-serif text-white font-extrabold uppercase">This is our royle scape</h1>
+            <p className="text-lg mb-5 uppercase text-white font-serif">our extraordinary view and shows</p>
+            <button className="px-8 bg-[#877a52] hover:bg-[#d3aa2f] duration-700 py-3 text-white">
+              Explore More
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Explore;
